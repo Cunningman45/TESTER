c2da3c091458bd2a8fa886209e8a3b74cc89fd37
Adam Cunningham <adamcunningham54@gmail.com>
2024-02-13 15:01:23 -0500

    First Legit Commit
    Added a slew of different things, animations and scripts to make a stick figure sprite run and jump about on a basic platform

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,41 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     public float moveSpeed = 5f;
     public float jumpForce = 10f;
 
     private Rigidbody2D rb;
     private bool isGrounded;
+    private Animator anim;
 
-    void Start()
-    {
+    void Start() {
+        //Grab references for rigidbody and animator from object
         rb = GetComponent<Rigidbody2D>();
-    }
+        anim = GetComponent<Animator>(); 
+    }//end Start
 
     void Update(){
         float moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && isGrounded){
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        //Flip player sprite when moving left or right
+        if(moveInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (moveInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded){
+            Jump();
         }//end if
+
+        //Set animation params
+        anim.SetBool("Run", moveInput != 0);
+        anim.SetBool("Grounded", isGrounded);
     }//end Update
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
+    private void Jump(){
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        anim.SetTrigger("Jump");
+        isGrounded = false;
+    }//end Jump
+
+    private void OnCollisionEnter2D(Collision2D collision){
         if (collision.gameObject.CompareTag("Ground")){
             isGrounded = true;
-        }
-    }
+        }//end if
+    }//end OnCollisionEnter2D
 
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
+    /*private void OnCollisionExit2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Ground")){
             isGrounded = false;
         }
-    }
-}
+    }//end OnCollisionExit2D*/
+}//end class PlayerController
 
