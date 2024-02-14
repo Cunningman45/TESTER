822eec93a95ab19fcba680d30a0d04f2b26d183a
Adam Cunningham <adamcunningham54@gmail.com>
2024-02-14 00:46:59 -0500

    Main Menu Commit
    Added an unfinished main menu scene with a button, as well as some extra camera features in the Sample Scene

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollowPlayer : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class CameraFollowPlayer : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+    //initialize variables
+    [SerializeField] private float speed;
+    private Vector3 velocity = Vector3.zero;
+
+    //variables so the camera follows the player, with variables to determine how far ahead of the player
+    //that the camera can see and how fast the camera adjusts to the players movements
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
+
+    void Update(){
+        //Makes the camera follow the player along the x axis
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+    }//end Update
+}//end class CameraFollowPlayer
